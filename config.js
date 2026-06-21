a1fd2d38ec6b9e128bfeb2ae2cd72ec0232dee33
ediaStudio <ediaStudio17@gmail.com>
2026-06-21 20:11:27 +0700

    ok

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const APP_CONFIG = {
 
   // ── ICÔNE ────────────────────────────────────────────────
   // Place ton icône dans /assets/ et indique le nom du fichier
-  iconFile: "assets/icon.jpg",   // ex: "assets/icon.png"
+  iconFile: "assets/icon.webp",   // ex: "assets/icon.png"
 
   // ── LIENS STORES ─────────────────────────────────────────
   appStoreUrl:  "https://apps.apple.com/us/app/id1497436522",
